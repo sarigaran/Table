07601836d52671ed2ce56663356406b4bb20588e
unknown <you@example.com>
2022-09-05 18:49:23 +0530

    card

--- myprojectapp/src/App.js
+++ myprojectapp/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/card/card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +18,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      
+      <Card/>
     </div>
+    </div>
+
   );
 }
 
